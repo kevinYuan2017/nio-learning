9b60a6314d79ec593de9b51ee538edaaa130f23c
kevinYuan2017 <33470613+kevinYuan2017@users.noreply.github.com>
2019-02-07 16:24:56 +0800

    init commit for timeserver by netty.

---
+++ timeserver-netty/src/main/java/com/kevin/learning/netty/timeservernetty/server/TimeServer.java
@@ -0,0 +1,54 @@
+package com.kevin.learning.netty.timeservernetty.server;
+
+import com.kevin.learning.netty.timeservernetty.server.handler.TimeServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+public class TimeServer {
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        }finally {
+            //优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 9000;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                // use default value
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
