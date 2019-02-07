d08183d5d1d69300cfd35e4f45631541923eb213
kevinYuan2017 <33470613+kevinYuan2017@users.noreply.github.com>
2019-02-07 15:14:23 +0800

    commit aio time client with some bug to be fixed.

--- timeserver-aio/src/main/java/com/kevin/netty/aio/timeclientaio/TimeClientAioApplication.java
+++ timeserver-aio/src/main/java/com/kevin/netty/aio/timeclientaio/TimeClientAioApplication.java
@@ -1,7 +1,19 @@
 package com.kevin.netty.aio.timeclientaio;
 
+
+import com.kevin.netty.aio.timeclientaio.thread.AsyncTimeClientHandler;
+
 public class TimeClientAioApplication {
     public static void main(String[] args) {
-
+        int port = 9000;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+                // use default value
+            }
+        }
+        System.out.println("port = " + port);
+        new Thread(new AsyncTimeClientHandler("127.0.0.1", port), "AIO-AsyncTimeClientHandler-001").start();
     }
 }
