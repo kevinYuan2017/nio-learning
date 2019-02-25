166935f13796868d39f37daec24e9f9fd397764b
kevinYuan2017 <33470613+kevinYuan2017@users.noreply.github.com>
2019-02-25 14:48:38 +0800

    初始提交httpfileserver

---
+++ httpfileserver/src/main/java/com/kevin/nio/learning/netty/httpfileserver/HttpfileserverApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.nio.learning.netty.httpfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpfileserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpfileserverApplication.class, args);
+	}
+
+}
---
+++ httpfileserver/src/test/java/com/kevin/nio/learning/netty/httpfileserver/HttpfileserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kevin.nio.learning.netty.httpfileserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpfileserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
