a3423f15547be8d2d667b7ca2bdcb4c68fc3b87f
yzk <zkyspace@163.com>
2019-04-15 13:06:31 +0800

    packaged Response method.

---
+++ httpserver/src/main/java/com/kevin/learning/netty/httpserver/Util/HttpResponseUtil.java
@@ -0,0 +1,33 @@
+package com.kevin.learning.netty.httpserver.Util;
+
+import com.alibaba.fastjson.JSON;
+import com.sun.istack.internal.NotNull;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.sun.deploy.net.HttpRequest.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Values.KEEP_ALIVE;
+import static io.netty.handler.codec.rtsp.RtspHeaders.Names.CONTENT_LENGTH;
+
+public class HttpResponseUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpResponseUtil.class);
+    public static void returnJson(ChannelHandlerContext ctx, HttpResponseStatus status, @NotNull Object data) {
+        String jsonString = JSON.toJSONString(data);
+        ByteBuf byteBuf = Unpooled.wrappedBuffer(jsonString.getBytes());
+
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status, byteBuf);
+        response.headers().set(CONTENT_TYPE, "application/json; charset=utf-8");
+        response.headers().set(CONNECTION, KEEP_ALIVE);
+        response.headers().set(CONTENT_LENGTH, jsonString.length());
+
+        LOGGER.info(response.toString());
+        ctx.writeAndFlush(response);
+    }
+}
--- httpserver/src/main/java/com/kevin/learning/netty/httpserver/handler/MyHttpRequestHandler.java
+++ httpserver/src/main/java/com/kevin/learning/netty/httpserver/handler/MyHttpRequestHandler.java
@@ -1,14 +1,10 @@
 package com.kevin.learning.netty.httpserver.handler;
 
-import com.alibaba.fastjson.JSON;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
+import com.kevin.learning.netty.httpserver.Util.HttpResponseUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.handler.codec.http.HttpVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,10 +12,6 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.sun.deploy.net.HttpRequest.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
-import static io.netty.handler.codec.http.HttpHeaders.Values.KEEP_ALIVE;
-import static io.netty.handler.codec.rtsp.RtspHeaders.Names.CONTENT_LENGTH;
 import static io.netty.handler.codec.spdy.SpdyHeaders.HttpNames.METHOD;
 
 @Component
@@ -31,15 +23,6 @@ public class MyHttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRe
         Map<String, String> returnMap = new HashMap<>();
         returnMap.put(METHOD, request.getMethod().name());
         returnMap.put("REQUEST_URI", request.getUri());
-        String jsonString = JSON.toJSONString(returnMap);
-        ByteBuf byteBuf = Unpooled.wrappedBuffer(jsonString.getBytes());
-
-        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, byteBuf);
-        response.headers().set(CONTENT_TYPE, "application/json; charset=utf-8");
-        response.headers().set(CONNECTION, KEEP_ALIVE);
-        response.headers().set(CONTENT_LENGTH, jsonString.length());
-        LOGGER.info(response.toString());
-
-        ctx.writeAndFlush(response);
+        HttpResponseUtil.returnJson(ctx, HttpResponseStatus.OK, returnMap);
     }
 }
